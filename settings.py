8d34e96c70ead8757aa6984515152a1543db53ba
WingsZeng <wings.xiangyi.zeng@gmail.com>
2022-08-21 08:43:41 +0800

    fix: Fix bugs in getSettings

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ def getSettings() -> sublime.Settings:
     """
     settings = sublime.load_settings('hugee.sublime-settings')
     if len(settings.get('site_path')) == 0:
+        window = sublime.active_window()
         project_data = window.extract_variables()
         folder_path = project_data['folder']
         settings.set('site_path', folder_path)
